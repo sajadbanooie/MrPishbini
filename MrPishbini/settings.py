66502f57ae9f51bf8c4488b32dc300145529859d
sajadbanooie <sajadbanooie@gmail.com>
2018-05-31 15:27:59 +0430

    fixed allowed hosts

--- MrPishbini/settings.py
+++ MrPishbini/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '76pr51eb-hu^cgmtn&d6cwhc*@*mwhes4!hfbwkn#br!vyebw4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
