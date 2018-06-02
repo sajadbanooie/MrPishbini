c744bcdf5650632052a1c153bd41992707679978
sajadbanooie <sajadbanooie@gmail.com>
2018-06-02 15:00:29 +0430

    bot code

--- MrPishbiniBot/bot.py
+++ MrPishbiniBot/bot.py
@@ -1,8 +1,13 @@
 from telegram import Update, Message
+from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
+from telegram import KeyboardButton
 from telegram.bot import Bot
 
-WELCOME_MSG = ''
+WELCOME_MSG = 'سلام خوش اومدین'
 START_COMMAND = '/start'
+RULES_COMMAND = ''
+MATCH_LIST_COMMAND = ''
+PISHBINI_COMMAND = ''
 
 
 class MrPishbiniBot:
@@ -15,4 +20,7 @@ class MrPishbiniBot:
 
     @staticmethod
     def start(bot: Bot, update: Update):
-        bot.send_message(update.message.chat_id, "hello")
+        bot.send_message(update.message.chat_id, WELCOME_MSG
+                         , reply_markup=ReplyKeyboardMarkup(
+                [[KeyboardButton(RULES_COMMAND), KeyboardButton(MATCH_LIST_COMMAND),
+                  KeyboardButton(PISHBINI_COMMAND)]]))
